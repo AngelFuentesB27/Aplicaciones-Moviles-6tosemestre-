6ef58e27a2d407021c860e7a2e2a7a4a621bc4fc
AngelFuentesB7 <fuentesbalderrama.angel2023@cbtis236.edu.mx>
2026-03-21 22:18:56 -0600

    dart

--- Dart/mascota.dart
+++ Dart/mascota.dart
@@ -5,16 +5,26 @@ class Mascota {
   double _peso;
 
   Mascota(this.nombre, this.especie, this._edad, this._peso);
-  void mostrar() {
-    print(
-      'Nombre:[$nombre] | Especie:[$especie] | Edad:[$_edad] años | Peso: [$_peso] kg',
-    );
-  }
+
+  int get edad => _edad;
+
+  double get peso => _peso;
+
+  bool get esCachorro => _edad < 2;
+
+  String get descripcion => '$nombre ($especie) | $_edad años | $_peso kg';
 }
 
 void main(List<String> args) {
-  var mascotaUno = Mascota('Rocky', 'Perro', 3, 8.5);
-  var mascotaDos = Mascota('Luna', 'gato', 1, 3.2);
-  mascotaUno.mostrar();
-  mascotaDos.mostrar();
+  var rocky = Mascota('Rocky', 'Perro', 3, 8.5);
+  var luna = Mascota('Luna', 'gato', 1, 3.2);
+  print(rocky.edad);
+  print(rocky.peso);
+  print(rocky.esCachorro);
+  print(rocky.descripcion);
+
+  print(luna.edad);
+  print(luna.peso);
+  print(luna.esCachorro);
+  print(luna.descripcion);
 }
