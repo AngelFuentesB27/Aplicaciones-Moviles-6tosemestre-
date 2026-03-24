2d36f6b47a3bb88ffe3e142af078167b45bb07b1
AngelFuentesB7 <fuentesbalderrama.angel2023@cbtis236.edu.mx>
2026-03-23 22:22:55 -0600

    dart

---
+++ Dart/tarea7p2.dart
@@ -0,0 +1,30 @@
+class Velocimetro {
+  double _velocidad;
+
+  Velocimetro() : _velocidad = 0.0;
+
+  double get velocidad => _velocidad;
+
+  set velocidad(double nuevoValor) {
+    if (nuevoValor >= 0.0 && nuevoValor <= 200.0) {
+      _velocidad = nuevoValor;
+    } else {
+      print('Velocidad inválida: $nuevoValor. Debe estar entre 0 y 200');
+    }
+  }
+
+  String get situacion {
+    if (_velocidad >= 150.0) return 'Peligroso';
+    if (_velocidad >= 100.0) return 'Rápido';
+    if (_velocidad >= 50.0) return 'Normal';
+    if (_velocidad > 0.0) return 'Lento';
+    return 'Estacionado';
+  }
+}
+
+void main(List<String> args) {
+  var velocidad = Velocimetro();
+  print(velocidad.velocidad);
+  velocidad.velocidad = 0.5;
+  print(velocidad.situacion);
+}
